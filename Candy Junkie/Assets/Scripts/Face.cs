37f4c094369bf2d037cf91fd6a21465beb427f1d
Chris Schack <Cwpwyt@gmail.com>
2020-08-28 06:32:36 -0400

    Added Simple Movement/ Face Animation

---
+++ Candy Junkie/Assets/Scripts/Face.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Face : MonoBehaviour
+{
+    //Params
+    [SerializeField] Sprite RightFace;
+    [SerializeField] Sprite LeftFace;
+
+    //Cached Values
+    SpriteRenderer sprite;
+
+    void Start()
+    {
+        //Get Cached Values
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    public void flipFace(float x)
+    {
+        //Checks Which Direction Player Is Going Then Change Face
+        if (x > 0)
+        {
+            sprite.sprite = RightFace;
+        }
+        else if (x < 0)
+        {
+            sprite.sprite = LeftFace;
+        }
+    }
+}
---
+++ Candy Junkie/Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Params
+    [SerializeField] float speed = 1;
+
+    //Declare Vars 
+    Face face;
+
+    //Cached Refrences
+    Rigidbody2D rb;
+    SpriteRenderer sprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get Cached Values Of Components
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+
+        //Set Object Vars Values
+        face = FindObjectOfType<Face>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get Movement Input
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        //Update Face
+        face.flipFace(x);
+
+        //Get Movement Values
+        float moveByX = x * speed;
+        float moveByY = y * speed;
+
+        //Update Movement
+        rb.velocity = new Vector2(moveByX, moveByY);
+
+    }
+}
