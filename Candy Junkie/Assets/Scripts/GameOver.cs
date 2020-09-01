fad958d912d038289acef2649a86b7c349ee2a3f
Chris Schack <Cwpwyt@gmail.com>
2020-09-01 10:52:03 -0400

    Added Timer

---
+++ Candy Junkie/Assets/Scripts/GameOver.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameOver : MonoBehaviour
+{
+    //Params
+    [SerializeField] TextMeshProUGUI TimerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get How Many Seconds The Run Has Taken
+        int timeOfRun = Mathf.RoundToInt(Time.time) - Mathf.RoundToInt(PlayerPrefs.GetFloat("Time Started"));
+
+        //Get Minutes/ Seconds
+        int Minutes = timeOfRun / 60;
+        int Seconds = timeOfRun % 60;
+
+        //Set Text
+        TimerText.SetText("Time Survived " + Minutes.ToString("d2") + ":" + Seconds.ToString("d2"));
+    }
+}
